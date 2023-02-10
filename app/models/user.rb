3b002185e6edb12d62dc55ef4b8a84be30705c7d
jkhbuild <jkh.build@gmail.com>
2023-02-10 17:52:30 -0500

    add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::Email_REGEXP }
+    validates :first_name, :last_name, :password_digest, :session_token, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 8 }, allow_nil: true
 end
