7e575e9ba8b237d71012e2cd656844396d4dd4d0
jkhbuild <jkh.build@gmail.com>
2023-02-11 19:36:22 -0500

    define current_user in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
     before_action :snake_case_params
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
     private
 
     def snake_case_params
