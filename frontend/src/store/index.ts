15d6d1f80c58d322b4f302d4070316a2f86c8f76
jkhbuild <jkh.build@gmail.com>
2023-02-12 15:49:38 -0500

    setup frontend for typescript and set up initial store folder

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import type { Middleware } from "redux"
+
+const rootReducer = combineReducers({})
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({
+
+})
+
+export default store;
